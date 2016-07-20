4c45ea3f34caf86fd926b09a6ddf8bb0d73a8de9
Kayla Fuchs <kaylafuchs@Kaylas-MacBook-Pro.local>
2016-07-20 13:33:59 -0400

    Updates 7/20/16

--- haiku_generator.js
+++ haiku_generator.js
@@ -1,23 +1,19 @@
-// haiku_generator should take two command line arguments: a structure (array of numbers)
-// and a document (file name of js file that sorts a document like cmu dictionary by syllable numbers)
-var structure = process.argv[2].replace(/\W/g,"").split("").map(Number);
+// haiku_generator takes two command line arguments: a structure (array made up of 3 arrays of numbers, one for each line)
+// and a document (file name of module that sorts a document like cmu dictionary by syllable numbers into a wordArray)
+// eg: node haiku_generator.js [[2,3],[4,3],[1,1,2,1]] sorted_cmu_dictionary.js
+
+var structure = eval(process.argv[2])
 var doc = require('./' + process.argv[3]).wordArray;
 
 function createHaiku(structure,doc){
 	var haiku = ""
-	var counter = 0;
-
-	structure.forEach(function(num){
-		var syllableArray = doc[num - 1]
-		haiku += " " + syllableArray[Math.floor(Math.random() * syllableArray.length)].replace(/[^a-zA-Z]/g,"");
-		counter += num;
-		if (counter == 5 || counter == 12 || counter == 17){
-			haiku += "\n";
-		}
+	structure.forEach(function(line){
+		line.forEach(function(num){
+			var syllableArray = doc[num - 1];
+			haiku += " " + syllableArray[Math.floor(Math.random() * syllableArray.length)].replace(/[^a-zA-Z]/g,"");
+		});
+		haiku += "\n";
 	});
-	if (counter != 17){
-		return "You entered " + counter + " syllables instead of 17, so this isn't really a haiku.\n" + haiku;
-	}
 	return haiku;
 }
 
---
+++ sorted_waste_land.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var wasteLand = readWasteLand('./the_waste_land.txt');
+var dictionary = require('./sorted_cmu_dictionary.js').wordArray;
+var sortedWasteLand = wasteLand.replace(/\W+/g," ").split(" ");
+var bookSyllableCounts = ""
+
+
+function readWasteLand(file){
+	return fs.readFileSync(file).toString();
+}
+
+function syllableCounter(doc){
+	doc.forEach(function(word){
+		for (var i = 0; i < dictionary.length; i++){
+			if (dictionary[i].indexOf(word.toUpperCase()) > -1){
+				bookSyllableCounts += (i + 1);
+			}
+		}
+	})
+	return bookSyllableCounts;
+}
+
+syllableCounter(sortedWasteLand);
+
+
+module.exports = {
+	sortedWasteLand: sortedWasteLand,
+	bookSyllableCounts: bookSyllableCounts
+}
+
+//console.log(bookSyllableCounts);
